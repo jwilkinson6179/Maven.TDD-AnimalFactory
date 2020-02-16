48456d013b598a331470f0b73f8801ed178417af
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-15 20:08:09 -0500

    Testing complete

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,12 +1,137 @@
 package rocks.zipcodewilmington;
 
+import com.sun.tools.corba.se.idl.InterfaceGen;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
+
+import java.awt.*;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author leon on 4/19/18.
  */
 public class CatHouseTest {
-    // TODO - Create tests for `void add(Cat cat)`
-    // TODO - Create tests for `void remove(Integer id)`
-    // TODO - Create tests for `void remove(Cat cat)`
-    // TODO - Create tests for `Cat getCatById(Integer id)`
-    // TODO - Create tests for `Integer getNumberOfCats()`
+    Cat testCatA;
+    Cat testCatB;
+    Dog testDog;
+    CatHouse testCatHouse;
+    DogHouse testDogHouse;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        CatHouse.clear();
+        DogHouse.clear();
+    }
+
+    @Test
+    public void AddACat()
+    {
+        // GIVEN
+
+        Integer expected = 2;
+        testCatA = new Cat("Ash", new Date(2018, 11, 8), 55);
+        testCatB = new Cat("Cinder", new Date(2018, 10, 31), 4);
+
+        // WHEN
+
+        testCatHouse.add(testCatA);
+        testCatHouse.add(testCatB);
+        Integer actual = testCatHouse.getNumberOfCats();
+
+        // THEN
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void RemoveCatByIDTest()
+    {
+        // GIVEN
+        Integer expectedBefore = 2;
+        Integer expectedAfter = 2;
+        testCatA = new Cat("Fleabag", new Date(2020, 1, 1), 4);
+        testCatB = new Cat("Dirtball", new Date(2019, 11, 4), 9);
+
+        // WHEN
+
+        testCatHouse.add(testCatA);
+        testCatHouse.add(testCatB);
+        Integer actualBefore = testCatHouse.getNumberOfCats();
+        testCatHouse.remove(4);
+        Integer actualAfter = testCatHouse.getNumberOfCats();
+
+        // THEN
+
+        assertEquals(expectedBefore, actualBefore);
+        assertEquals(expectedAfter, expectedAfter);
+    }
+
+    @Test
+    public void RemoveCatByCatTest()
+    {
+        // GIVEN
+        Integer expectedBefore = 2;
+        Integer expectedAfter = 2;
+        testCatA = new Cat("Fluff", new Date(2017, 8, 65), 14);
+        testCatB = new Cat("Snuffle", new Date(2019, 3, 155), 19);
+
+        // WHEN
+
+        testCatHouse.add(testCatA);
+        testCatHouse.add(testCatB);
+        Integer actualBefore = testCatHouse.getNumberOfCats();
+        testCatHouse.remove(testCatA);
+        Integer actualAfter = testCatHouse.getNumberOfCats();
+
+        // THEN
+
+        assertEquals(expectedBefore, actualBefore);
+        assertEquals(expectedAfter, expectedAfter);
+    }
+
+    @Test
+    public void GetCatByID() {
+        // GIVEN
+
+        testCatA = new Cat("Mad Max", new Date(2008, 5, 31), 6179);
+
+        // WHEN
+
+        testCatHouse.add(testCatA);
+        Cat actual = testCatHouse.getCatById(6179);
+
+        // THEN
+
+        assertEquals(testCatA, actual);
+    }
+
+    @Test
+    public void GetNumberOfCat() {
+        // GIVEN
+
+        Integer expected = 0;
+
+        // WHEN
+
+        Integer actual = testCatHouse.getNumberOfCats();
+
+        // THEN
+
+        assertEquals(expected, actual);
+    }
 }
--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,34 +1,127 @@
 package rocks.zipcodewilmington;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Dog;
-import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
-
+import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
 import java.util.Date;
+import static org.junit.Assert.assertEquals;
 
-/**
- * @author leon on 4/19/18.
- */
-public class DogHouseTest {
-    // TODO - Create tests for `void add(Dog dog)`
-    // TODO - Create tests for `void remove(Integer id)`
-    // TODO - Create tests for `void remove(Dog dog)`
-    // TODO - Create tests for `Dog getDogById(Integer id)`
-    // TODO - Create tests for `Integer getNumberOfDogs()`
+    /**
+     * @author leon on 4/19/18.
+     */
+    public class DogHouseTest {
+        Dog testDogA;
+        Dog testDogB;
+        DogHouse testDogHouse;
 
-    @Test
-    public void testGetNumberOfDogs() {
-        // Given (some
-        String name = "Milo";
-        Date birthDate = new Date();
-        Dog animal = AnimalFactory.createDog(name, birthDate);
-        DogHouse.clear();
+        @Before
+        public void setUp() throws Exception {
 
-        // When
-        DogHouse.add(animal);
+        }
 
-        // Then
-        DogHouse.getNumberOfDogs();
+        @After
+        public void tearDown() throws Exception {
+            DogHouse.clear();
+            DogHouse.clear();
+        }
+
+        @Test
+        public void AddADog()
+        {
+            // GIVEN
+
+            Integer expected = 2;
+            testDogA = new Dog("Scraps", new Date(2018, 11, 8), 55);
+            testDogB = new Dog("Milo", new Date(2018, 10, 31), 4);
+
+            // WHEN
+
+            testDogHouse.add(testDogA);
+            testDogHouse.add(testDogB);
+            Integer actual = testDogHouse.getNumberOfDogs();
+
+            // THEN
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void RemoveDogByIDTest()
+        {
+            // GIVEN
+            Integer expectedBefore = 2;
+            Integer expectedAfter = 2;
+            testDogA = new Dog("Itchy", new Date(2020, 1, 1), 4);
+            testDogB = new Dog("Scratchy", new Date(2019, 11, 4), 9);
+
+            // WHEN
+
+            testDogHouse.add(testDogA);
+            testDogHouse.add(testDogB);
+            Integer actualBefore = testDogHouse.getNumberOfDogs();
+            testDogHouse.remove(4);
+            Integer actualAfter = testDogHouse.getNumberOfDogs();
+
+            // THEN
+
+            assertEquals(expectedBefore, actualBefore);
+            assertEquals(expectedAfter, expectedAfter);
+        }
+
+        @Test
+        public void RemoveDogByDogTest()
+        {
+            // GIVEN
+            Integer expectedBefore = 2;
+            Integer expectedAfter = 2;
+            testDogA = new Dog("Spike", new Date(2017, 8, 65), 14);
+            testDogB = new Dog("Kujo", new Date(2019, 3, 155), 19);
+
+            // WHEN
+
+            testDogHouse.add(testDogA);
+            testDogHouse.add(testDogB);
+            Integer actualBefore = testDogHouse.getNumberOfDogs();
+            testDogHouse.remove(testDogA);
+            Integer actualAfter = testDogHouse.getNumberOfDogs();
+
+            // THEN
+
+            assertEquals(expectedBefore, actualBefore);
+            assertEquals(expectedAfter, expectedAfter);
+        }
+
+        @Test
+        public void GetDogByID() {
+            // GIVEN
+
+            testDogA = new Dog("Mr. Ruff", new Date(2008, 5, 31), 6179);
+
+            // WHEN
+
+            testDogHouse.add(testDogA);
+            Dog actual = testDogHouse.getDogById(6179);
+
+            // THEN
+
+            assertEquals(testDogA, actual);
+        }
+
+        @Test
+        public void GetNumberOfDog() {
+            // GIVEN
+
+            Integer expected = 0;
+
+            // WHEN
+
+            Integer actual = testDogHouse.getNumberOfDogs();
+
+            // THEN
+
+            assertEquals(expected, actual);
+        }
     }
-}
