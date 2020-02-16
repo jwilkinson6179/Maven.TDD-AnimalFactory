57b955577f4e98d9d1aaf9d82945e75ccaffb353
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-15 19:19:50 -0500

    Factory tested

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,28 +1,67 @@
 package rocks.zipcodewilmington;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author leon on 4/19/18.
  */
-/*
+
 public class AnimalFactoryTest {
 
     Dog testDog;
     Cat testCat;
+    AnimalFactory testFactory;
 
     @Before
+    public void setUp() throws Exception {
+    }
 
     @After
+    public void tearDown() throws Exception {
+    }
 
     @Test
-    //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
-    @Test
-    //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+    public void FactoryMakesADogTest() {
+        // GIVEN
 
+        testFactory = new AnimalFactory();
+        String givenName = "RoboDog";
+        Date givenDate = new Date(2020, 2, 15);
+
+        // WHEN
+
+        Animal actual = testFactory.createDog(givenName, givenDate);
+
+        // THEN
+
+        assertTrue(actual instanceof Dog);
+    }
+
+    @Test
+    public void FactoryMakesACatTest() {
+        // GIVEN
+
+        testFactory = new AnimalFactory();
+        String givenName = "CyberKitty";
+        Date givenDate = new Date(2020, 2, 14);
+
+        // WHEN
+
+        Animal actual = testFactory.createCat(givenName, givenDate);
+
+        // THEN
+
+        assertTrue(actual instanceof Cat);
+    }
 }
-*/
--- src/test/java/rocks/zipcodewilmington/DogTest.java
+++ src/test/java/rocks/zipcodewilmington/DogTest.java
@@ -5,10 +5,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.Mammal;
 
 import java.util.Date;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * @author leon on 4/19/18.
@@ -88,17 +89,97 @@ public class DogTest {
     public void SetBDayTest()
     {
         // GIVEN
-
+        Date expected = new Date(2008, 6, 6);
+        String givenName = "Roofus";
+        Date givenBDay = new Date(2016, 5, 1);
+        Integer givenId = 55;
+        testDog = new Dog(givenName, givenBDay, givenId);
+        Date newBDay = new Date (2008, 6, 6);
         // THEN
+
+        testDog.setBirthDate(newBDay);
+        Date actual = testDog.getBirthDate();
 
         // WHEN
 
-        assertEquals(1, 2);
+        assertEquals(expected, actual);
     }
 
-    // TODO - Create tests for `setBirthDate(Date birthDate)`
-    // TODO - Create tests for `void eat(Food food)`
-    // TODO - Create tests for `Integer getId()`
-    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
-    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+    @Test
+    public void EatFoodTwiceTest()
+    {
+        // GIVEN
+
+        Integer expected = 2;
+        Food givenMeal = new Food();
+        String givenName = "Puggles";
+        Date givenDate = new Date(2018, 3, 4);
+        Integer givenId = 1122;
+        testDog = new Dog(givenName, givenDate, givenId);
+
+        // WHEN
+
+        testDog.eat(givenMeal);
+        testDog.eat(givenMeal);
+        Integer actual = testDog.getNumberOfMealsEaten();
+        // THEN
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void GetIdTest()
+    {
+        // GIVEN
+
+        Integer expected = 666;
+        String givenName = "Devildog";
+        Date givenBDay = new Date(1066, 6, 6);
+        Integer givenId = 666;
+        testDog = new Dog(givenName, givenBDay, givenId);
+
+        // WHEN
+
+        Integer actual = testDog.getId();
+
+        // THEN
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void IsItADogTest()
+    {
+        // GIVEN
+
+        String givenName = "I'm Not a Dog!";
+        Date BDay = new Date(1979, 6, 1);
+        Integer givenId = 1;
+        testDog = new Dog(givenName, BDay, givenId);
+
+        // WHEN
+
+        Boolean actual = (testDog instanceof Dog);
+        // THEN
+
+        assertTrue(actual);
+    }
+
+    @Test
+    public void IsItAMammalTest()
+    {
+        // GIVEN
+
+        String givenName = "OK I Am A Mammal";
+        Date BDay = new Date(1986, 2, 11);
+        Integer givenId = 122;
+        testDog = new Dog(givenName, BDay, givenId);
+
+        // WHEN
+
+        Boolean actual = (testDog instanceof Mammal);
+        // THEN
+
+        assertTrue(actual);
+    }
 }
